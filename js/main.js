a99c81324a7e2a9ca434469fa8d1816e4b5a54a1
Roberto Rivera Reyes <roberto.rivera16@upr.edu>
2016-09-26 11:15:47 -0400

    SinglePage

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
 var app = {
 
+  showAlert: function(message, title) {
+    if(navigator.notification){
+      navigator.notification.alert(message, null, title, 'OK');
+    }else{
+      alert(title ? (title + ": " + message) : message);
+    }
+  },
+
+  renderHomeView: function() {
+      var html =
+              "<div class='header'><h1>Home</h1></div>" +
+              "<div class='search-view'>" +
+              "<input class='search-key'/>" +
+              "<ul class='employee-list'></ul>" +
+              "</div>"
+            $('body').html(html);
+      $('.search-key').on('keyup', $.proxy(this.findByName, this));
+  },
+
     findByName: function() {
         console.log('findByName');
         this.store.findByName($('.search-key').val(), function(employees) {
@@ -16,17 +35,9 @@ var app = {
     initialize: function() {
         var self = this;
         this.store = new LocalStorageStore(function() {
-          self.showAlert('Store Initialized', 'Info');
+          self.renderHomeView();
     });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
-
-    showAlert: function(message, title) {
-      if(navigator.notification){
-        navigator.notification.alert(message, null, title, 'OK');
-      }else{
-        alert(title ? (title + ": " + message) : message);
-      }
     }
 
 
