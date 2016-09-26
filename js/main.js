e48527091ab35d87f0d082adf360185fd49efee4
Roberto Rivera Reyes <roberto.rivera16@upr.edu>
2016-09-26 15:20:47 -0400

    Reset

--- js/main.js
+++ js/main.js
@@ -1,36 +1,33 @@
 var app = {
 
-
-
-  showAlert: function(message, title) {
-    if(navigator.notification){
-      navigator.notification.alert(message, null, title, 'OK');
-    }else{
-      alert(title ? (title + ": " + message) : message);
-    }
-  },
-
-  renderHomeView: function() {
-      $('body').html(this.homeTpl);
-      $('.search-key').on('keyup', $.proxy(this.findByName, this));
-  },
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
 
     findByName: function() {
-      var self = this;
-      this.store.findByName($('.search-key').val(), function(employees) {
-          $('.employee-list').html(self.employeeLiTpl(employees));
-      });
+        var self = this;
+        this.store.findByName($('.search-key').val(), function(employees) {
+            $('.employee-list').html(self.employeeLiTpl(employees));
+        });
+    },
+
+    renderHomeView: function() {
+        $('body').html(this.homeTpl());
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     },
 
     initialize: function() {
         var self = this;
         this.homeTpl = Handlebars.compile($("#home-tpl").html());
         this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-        this.store = new LocalStorageStore(function() {
-          self.renderHomeView();
+        this.store = new MemoryStore(function() {
+            self.renderHomeView();
         });
     }
-
 
 };
 
