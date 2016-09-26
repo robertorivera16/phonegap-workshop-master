5a1e1672a8fe79e5df8fb8ea92a203493fd29b61
Roberto Rivera Reyes <roberto.rivera16@upr.edu>
2016-09-26 14:49:44 -0400

    Handlebars Template

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 var app = {
 
+  this.homeTpl = Handlebars.compile($("#home-tpl").html());
+  this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+
   showAlert: function(message, title) {
     if(navigator.notification){
       navigator.notification.alert(message, null, title, 'OK');
@@ -9,34 +12,22 @@ var app = {
   },
 
   renderHomeView: function() {
-      var html =
-              "<div class='header'><h1>Home</h1></div>" +
-              "<div class='search-view'>" +
-              "<input class='search-key'/>" +
-              "<ul class='employee-list'></ul>" +
-              "</div>"
-            $('body').html(html);
+      $('body').html(this.homeTpl);
       $('.search-key').on('keyup', $.proxy(this.findByName, this));
   },
 
     findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
+      var self = this;
+      this.store.findByName($('.search-key').val(), function(employees) {
+          $('.employee-list').html(self.employeeLiTpl(employees));
+      });
     },
 
     initialize: function() {
         var self = this;
         this.store = new LocalStorageStore(function() {
           self.renderHomeView();
-    });
+        });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
